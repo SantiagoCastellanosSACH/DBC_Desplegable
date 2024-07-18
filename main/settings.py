03b9d1c11bbb49b3f4664705855496ba4989d5c9
SantiagoCastellanosSACH <senasantiagocastellanos@gmail.com>
2024-07-18 15:08:53 -0500

    Changes from VSC

--- main/settings.py
+++ main/settings.py
@@ -102,15 +102,15 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
 
-        #'ENGINE': 'django.db.backends.mysql',
-        #'NAME': 'dbc_prueba',
-        #'USER': 'root',
-        #'PASSWORD': '',
-        #'HOST': '127.0.0.1', 
-        #'PORT': '3306',   
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbc_prueba',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1', 
+        'PORT': '3306',   
     }
 }
 
